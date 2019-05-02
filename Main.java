431e87498fbecc1ddf714efadc06fbbe9357cef3
JDanel <julie.danel@isen.yncrea.fr>
2019-05-02 10:11:28 +0200

    add a map with cities and roads

--- Main.java
+++ Main.java
@@ -1,9 +1,6 @@
 import engine.processing.InitParse;
 
 import org.w3c.dom.Document;
-import org.w3c.dom.NodeList;
-import org.w3c.dom.Node;
-import org.w3c.dom.Element;
 
 import java.util.Map;
 import java.util.HashMap;
@@ -11,15 +8,13 @@ import java.util.Vector;
 import java.util.Collections;
 
 import engine.entities.city.*;
-import engine.entities.utils.*;
 import engine.entities.ways.*;
-import engine.entities.vehicle.*;
 
 
 public class Main {
     public static void main(String args[]) {
         InitParse parser = new InitParse();
-        Document doc = parser.parseInitFile("./InitStructure.xml");
+        Document doc = parser.parseInitFile("maps/InitStructure.xml");
 
         Vector<City> cities = new Vector<City>();
         Vector<Road> roads = new Vector<Road>();
