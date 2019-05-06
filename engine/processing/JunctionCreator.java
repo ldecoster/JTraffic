ed2dce2eda6cf11a3e09c5daa60f5d6e1e2c4af6
BDeliers <contact@bdeliers.com>
2019-05-06 10:52:15 +0200

    Tried to excluse wrong junctions

--- engine/processing/JunctionCreator.java
+++ engine/processing/JunctionCreator.java
@@ -10,6 +10,7 @@ public class JunctionCreator {
     public void createJunctions(Vector<Road> roads, Vector<Junction> junctions) {
 
         Vector<Float> v1, v2;
+        float xmin, xmax, ymin, ymax;
 
         for (int i = 0; i < roads.size(); i++) {
             if (roads.get(i).getType() != EnumRoad.HIGHWAY) {
@@ -17,20 +18,29 @@ public class JunctionCreator {
                 v1 = roads.get(i).computeEquation();
 
                 for (int j = i; j < roads.size(); j++) {
-                    if (i != j) {
+                    if (i != j && roads.get(j).getType() != EnumRoad.HIGHWAY) {
 
                         v2 = roads.get(j).computeEquation();
                         float x = (v2.get(1) - v1.get(1)) / (v1.get(0) - v2.get(0));
                         float y = v1.get(0) * x + v1.get(1);
 
                         if (x > 0 && x < 800 && y > 0 && y < 600) {
-                            System.out.println("(" + x + "," + y + ")");
 
-                            Junction newJunction = new Junction();
-                            Coords coords = new Coords((int) x, (int) y);
-                            newJunction.setCoordinates(coords);
+                            // Exclude the extremities
+                            xmin = Math.min(Math.min(roads.get(i).getDeparture().getCoordinates().getX(), roads.get(j).getDeparture().getCoordinates().getX()), Math.min(roads.get(i).getArrival().getCoordinates().getX(), roads.get(j).getArrival().getCoordinates().getX()));
+                            xmax = Math.max(Math.max(roads.get(i).getDeparture().getCoordinates().getX(), roads.get(j).getDeparture().getCoordinates().getX()), Math.max(roads.get(i).getArrival().getCoordinates().getX(), roads.get(j).getArrival().getCoordinates().getX()));
+                            ymin = Math.min(Math.min(roads.get(i).getDeparture().getCoordinates().getY(), roads.get(j).getDeparture().getCoordinates().getY()), Math.min(roads.get(i).getArrival().getCoordinates().getX(), roads.get(j).getArrival().getCoordinates().getX()));
+                            ymax = Math.max(Math.max(roads.get(i).getDeparture().getCoordinates().getY(), roads.get(j).getDeparture().getCoordinates().getY()), Math.max(roads.get(i).getArrival().getCoordinates().getX(), roads.get(j).getArrival().getCoordinates().getX()));
 
-                            junctions.add(newJunction);
+                            if(x > xmin && x < xmax && y > ymin && y < ymax) {
+                                // System.out.println("(" + x + "," + y + ")");
+
+                                Junction newJunction = new Junction();
+                                Coords coords = new Coords((int) x, (int) y);
+                                newJunction.setCoordinates(coords);
+
+                                junctions.add(newJunction);
+                            }
                         }
 
 
@@ -38,6 +48,8 @@ public class JunctionCreator {
                 }
             }
         }
+        // Remove occurencies
+        junctions = new Vector(new java.util.HashSet(junctions));
 
     }
 }
